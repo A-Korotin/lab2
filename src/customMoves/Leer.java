db805535070ccb1a1ad5243f247fe5a7b8c8b871
A-Korotin <81831713+A-Korotin@users.noreply.github.com>
2021-10-24 12:51:53 +0300

    refactor

--- src/customMoves/Leer.java
+++ src/customMoves/Leer.java
@@ -4,18 +4,17 @@ import java.lang.Math;
 
 public class Leer extends StatusMove
 {
-  public Leer()
-  { super(Type.NORMAL, 0, 100); }
+  public Leer(){
+    super(Type.NORMAL, 0, 100);
+  }
 
   @Override
-  protected void applyOppEffects(Pokemon p)
-  {
+  protected void applyOppEffects(Pokemon p){
     p.setMod(Stat.DEFENSE, -1);
   }
 
   @Override
-  protected String describe()
-  {
+  protected String describe(){
     return "Uses Leer";
   }
 
