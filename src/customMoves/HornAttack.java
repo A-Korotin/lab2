4f436f49484e516dc907674cef33fd5e15be58bf
A-Korotin <81831713+A-Korotin@users.noreply.github.com>
2021-10-24 12:51:05 +0300

    refactor

--- src/customMoves/HornAttack.java
+++ src/customMoves/HornAttack.java
@@ -4,12 +4,12 @@ import java.lang.Math;
 
 public class HornAttack extends PhysicalMove
 {
-  public HornAttack()
-    { super(Type.NORMAL, 65, 100); }
+  public HornAttack(){
+    super(Type.NORMAL, 65, 100);
+  }
 
   @Override
-  protected String describe()
-  {
+  protected String describe(){
     return "Uses HornAttack";
   }
 }
