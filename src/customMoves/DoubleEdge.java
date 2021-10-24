0c352b7e9109bd5bdda2e7bfde12928d2a6a6184
A-Korotin <81831713+A-Korotin@users.noreply.github.com>
2021-10-24 12:48:29 +0300

    refactor

--- src/customMoves/DoubleEdge.java
+++ src/customMoves/DoubleEdge.java
@@ -4,18 +4,17 @@ import java.lang.Math;
 
 public class DoubleEdge extends PhysicalMove
 {
-  public DoubleEdge()
-  { super(Type.NORMAL, 120, 100); }
+  public DoubleEdge(){
+    super(Type.NORMAL, 120, 100);
+  }
 
   @Override
-  protected void applySelfDamage(Pokemon p , double damage)
-  {
+  protected void applySelfDamage(Pokemon p , double damage){
     p.setMod(Stat.HP, (int) (damage / 3.0));
   }
 
   @Override
-  protected String describe()
-  {
+  protected String describe(){
     return "Uses DoubleEdge";
   }
 }
